4b5f4c6cf2bacdf3fab8ea51a2f6f8c5e421aaf0
eager-monitor-013e <phil.haines92@gmail.com>
2020-12-23 00:22:58 +0000

    Automatically backed up by Learn

--- lib/updating_nested_hashes.rb
+++ lib/updating_nested_hashes.rb
@@ -61,7 +61,7 @@ def add_characters
    # Code your solution here:
    
    epic_tragedy[:additional_characters] => [
-     {:name => "Prince Escalus"}
+     {:name => "Prince Escalus"},
      {:name => "Apothecary"
      ]
 
